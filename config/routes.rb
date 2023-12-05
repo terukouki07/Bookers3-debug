b87ae762edfc3f357abe672914a1f6e69f9f0ed9
EC2 Default User <ec2-user@ip-172-31-36-193.ap-northeast-1.compute.internal>
2023-12-05 01:59:51 +0000

    finish[フォロー機能]

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
     resource :favorite, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update] do
+  resources :users, only: [:index,:show,:edit,:updat,] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
