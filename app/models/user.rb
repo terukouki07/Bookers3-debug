e7fdcdc34549491dbad80b8d3cf1baf824b6b7eb
EC2 Default User <ec2-user@ip-172-31-36-193.ap-northeast-1.compute.internal>
2023-12-06 08:24:29 +0000

    [finish]検索機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,4 +41,17 @@ end
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
+  # 検索機能
+  def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
+    else
+      User.where('name LIKE ?', '%' + content + '%')
+    end
+  end
 end
